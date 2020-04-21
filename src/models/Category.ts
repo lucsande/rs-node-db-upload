6b3c9ce931fe34f17077ae12596b9c27f958e0f6
lucas sandeville <lucas.sandeville@gmail.com>
2020-04-21 10:55:07 -0300

    remove tsconfig warnings

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,4 +1,5 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import Transaction from './Transaction'
 
 @Entity('categories')
 class Category {
@@ -7,6 +8,9 @@ class Category {
 
   @Column()
   title: string;
+
+  @OneToMany(() => Transaction, transaction => transaction.category) 
+  transaction: Transaction[];
 
   @Column('timestamp with time zone')
   created_at: Date;
